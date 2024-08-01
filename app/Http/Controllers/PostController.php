f08b69b8c359875ee43694ad145a92e569af44aa
Edward <edwardevbert@gmail.com>
2024-08-01 10:00:39 +0700

    update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use App\Models\Post;
-use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
 
 class PostController extends Controller
